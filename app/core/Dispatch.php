c57d8e26290c165303939cf24b4033327dc20392
Jesse <jesseboer@hotmail.com>
2021-04-20 22:01:49 +0200

    initial commit

--- app/core/Dispatch.php
+++ app/core/Dispatch.php
@@ -8,13 +8,14 @@ class Dispatch {
 
     private Request $router;
     protected $params = [];
+    protected $url;
     private $controller; //Assign class to property
     private $method;
 
     public function __construct()
     {
         $this->router = new Request();
-        $this->params = array_values(array_filter(explode('/', $this->router->getPath())));
+        $this->url = array_values(array_filter(explode('/', $this->router->getPath())));
     }
 
     public function dispatch()
@@ -31,7 +32,8 @@ class Dispatch {
            echo 'Class does not exists!';
         }
 
-//        empty($this->params) ? $this->controller : $this->params;
-
+        $this->params = $this->url ? array_values($this->url) : [];
+        dump($this->params);
+        return call_user_func_array([$this->controller, $this->method], $this->params);
     }
 }
